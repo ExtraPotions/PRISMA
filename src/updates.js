54b79373f7b63134879f1896bc2ca2944c0359db
expDARE <5340947+ExtraPotions@users.noreply.github.com>
2026-09-25 15:29:54 -0700

    Pin shared update metadata endpoint

--- src/updates.js
+++ src/updates.js
@@ -1,6 +1,7 @@
 EXP.Updates = ExtraPotionsCore.createReleaseUpdateChecker({
   productId: 'prisma',
   repository: 'ExtraPotions/PRISMA',
+  endpoint: 'https://api.github.com/repos/ExtraPotions/PRISMA/releases/latest',
   currentVersion: EXP.VERSION,
   enabled: () => EXP.Settings.snapshot().updateNotifications,
   onError: error => EXP.Core.safeError(Object.assign(error, { code: 'UPDATE_CHECK' }), 'prisma'),
